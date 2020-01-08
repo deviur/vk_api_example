16b71e81e5f5c1b554278539770f1c7e1329e64a
deviur <deviur@yandex.ru>
2020-01-08 22:55:19 +0300

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -1,15 +1,18 @@
+import datetime
 import getpass
-import time
+import os
 import vk
 
 APP_ID = '6478436'
 V = 9.85
 # LIST_TO_SEND = ['deleted', 'relation', 'status', 'deactivated']
-LIST_TO_SEND = ['deleted', 'relation', 'deactivated']
+INCLUDE = ['deleted', 'relation', 'deactivated']    # Свойства пользователя, которые отслеживать
+EXCLUDE = ['track_code', 'online', 'status_audio']  # Свойства пользователя, которые не отслеживать
 USER_ID = 133147577
+REPORT_TIME = datetime.datetime.now()
+
 VERSION = '1.0.00 (08.01.2020)'
 AUTHOR = 'Deviur (https://github.com/deviur)'
-
 
 
 def login_by_password():
@@ -35,7 +38,7 @@ def login_by_token():
         access_token = file.readline()
         session = vk.Session(access_token=access_token)
 
-    api = vk.API(session, scope='messages', v=V)
+    api = vk.API(session, v=V)
     try:
         api.users.get()
     except vk.exceptions.VkAPIError:
@@ -50,23 +53,24 @@ def main():
     # Подключение к API
     api = vk.API(session, v=V)
 
-    # Проверка, удалась ли авторизация
-    try:
-        user = api.users.get()[0]
-    except vk.exceptions.VkAPIError:
-        print('Авторизация не удалась. Повторите попытку.\n')
-        login_by_password()
-    else:
-        print("Приветствую, {}!".format(user['first_name']))
-        print("Авторизация прошла успешно!")
+    # # Проверка, удалась ли авторизация
+    # try:
+    #     user = api.users.get()[0]
+    # except vk.exceptions.VkAPIError:
+    #     print('Авторизация не удалась. Повторите попытку.\n')
+    #     login_by_password()
+    # else:
+    #     print("Приветствую, {}!".format(user['first_name']))
+    #     print("Авторизация прошла успешно!")
 
-    friends = api.friends.get(user_id=0, fields='sex, bdate, city, relation, status')
+    friends_to_save = api.friends.get(user_id=0, fields='sex, bdate, city, relation, status')
+    print(REPORT_TIME)
 
     try:  # Проверяем наличие файла со списком друзей
         file = open("friends.ini", 'r')
     except IOError:
         file = open("friends.ini", 'w')
-        file.writelines(str(friends) + '\n')
+        file.writelines(str(friends_to_save) + '\n')
         return
 
     # Загружаем список друзей за предыдущий период из файла в словарь
@@ -74,7 +78,7 @@ def main():
     old_friends = eval(file.read())
 
     # Создаём словарь текущих друзей
-    keys, items = [f['id'] for f in friends['items']], [f for f in friends['items']]
+    keys, items = [f['id'] for f in friends_to_save['items']], [f for f in friends_to_save['items']]
     friends = dict(zip(keys, items))
 
     # Создаём словарь друзей предыдущего периода
@@ -84,20 +88,19 @@ def main():
     deleted = [f for f in old_friends.keys() if not(f in friends.keys())]
     added = [f for f in friends.keys() if f not in old_friends.keys()]
     changed = []
-    for f in friends.keys():
-        if f not in added:
-            friends[f].pop('track_code')
-            old_friends[f].pop('track_code')
-            friends[f].pop('online')
-            old_friends[f].pop('online')
+    for id in friends.keys():
+        if id not in added:
+            for item in EXCLUDE:
+                friends[id].pop(item, None)
+                old_friends[id].pop(item, None)
 
-            if friends[f] != old_friends[f]:
-                changed.append(f)
+            if friends[id] != old_friends[id]:
+                changed.append(id)
 
     message = ''
-    for d in deleted:
-        if 'deleted' in LIST_TO_SEND:
-            message += '\nПользовател(ь/и) удалил(ся/ись):\n'
+    if len(deleted) > 0 and 'deleted' in INCLUDE:
+        message += '\nПользовател(ь/и) удалил(ся/ись):\n'
+        for d in deleted:
             message += f'http://vk.com/id{d}\n'
 
     print(deleted)
@@ -107,23 +110,28 @@ def main():
     # Печать изменений
     message += '\nПользовател(ь/и) внес(ли) изменения:\n'
     for c in changed:
-        print("Изменения:", c)
+        print(f"Было:  {old_friends[c]}")
+        print(f"Стало: {friends[c]}")
         for key in friends[c]:
             if key not in old_friends[c]:
-                print("Было:  ", key, ":  -")
-                print("Стало: ", key, ":", friends[c][key])
-                if key in LIST_TO_SEND:
+                # print("Было:  ", key, ":  -")
+                # print("Стало: ", key, ":", friends[c][key])
+                if key in INCLUDE:
                     message += f'http://vk.com/id{c}\n'
                     message += f'Было:  {key}: -\n'
                     message += f'Стало: {key}: {friends[c][key]}\n'
             elif friends[c][key] != old_friends[c][key]:
-                print("Было:  ", key, ":", old_friends[c][key])
-                print("Стало: ", key, ":",  friends[c][key])
-                if key in LIST_TO_SEND:
+                # print("Было:  ", key, ":", old_friends[c][key])
+                # print("Стало: ", key, ":",  friends[c][key])
+                if key in INCLUDE:
                     message += f'http://vk.com/id{c}\n'
                     message += f'Было:  {key}: {old_friends[c][key]}\n'
                     message += f'Стало: {key}: {friends[c][key]}\n'
+
     print(message)
+
+    file = open("friends.ini", 'w')
+    file.writelines(str(friends_to_save) + '\n')
 
 
 if __name__ == "__main__":
